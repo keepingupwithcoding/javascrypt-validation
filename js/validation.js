956c09cdf319a37869c74ca853eafbf8bf783af8
Ompahslu Kua <haluskua.one@gmail.com>
2016-04-13 10:02:15 +1200

    start validation

---
+++ js/validation.js
@@ -0,0 +1,18 @@
+// console.log("Hello");
+
+//Find the form
+var registrationForm = document.querySelector('#registration');
+
+														// Wait for the user to submit the form
+registrationForm.onsubmit = function(event) {
+
+		// Stop the form from submitting
+		event.preventDefault();
+
+	// alert('submitted');
+
+
+}
+
+
+
