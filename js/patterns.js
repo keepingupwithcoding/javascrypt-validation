407b6d4cff8bc37f71f1a02824d8d865629ea47b
Ompahslu Kua <haluskua.one@gmail.com>
2016-04-15 10:15:44 +1200

    Add more validation

---
+++ js/patterns.js
@@ -0,0 +1,3 @@
+	// Patterns to use in validation
+	var usernamePattern = new RegExp("^[a-zA-Z0-9._-]{3,20}$");
+	var namePattern = new RegExp("^[a-zA-Z' .-]{1,30}$")
--- js/validation.js
+++ js/validation.js
@@ -6,9 +6,9 @@ var registrationForm = document.querySelector("#registration");
 														// Wait for the user to submit the form
 registrationForm.onsubmit = function(event) {
 
-	// Patterns to use in validation
-	var usernamePattern = new RegExp("^[a-zA-Z0-9._-]{3,20}$");
-	var namePattern = new RegExp("^[a-zA-Z' .-]{1,30}$")
+
+	// Count how man errors there are
+	var totalErrors = 0;
 
 	// Get references to the input fields
 	var usernameInput = document.querySelector("#username");
@@ -18,6 +18,14 @@ registrationForm.onsubmit = function(event) {
 	var nameInput = document.querySelector("#full-name");
 
 	var nameMessage = document.querySelector("#full-name-message");
+
+	var campusMessage = document.querySelector("#campus-message");
+
+	var campusOptions = document.querySelectorAll("[name=campus]");
+
+	console.log(campusOptions);
+
+
 
 
 	
@@ -31,6 +39,7 @@ registrationForm.onsubmit = function(event) {
 			//username is invalid
 			// console.log("Username is invalid");
 			usernameMessage.innerHTML = "Username is invalid";  //you can be more specific but okay for now
+			totalErrors++;
 		}
 	
 	// Check the username
@@ -42,11 +51,36 @@ registrationForm.onsubmit = function(event) {
 			//username is invalid
 			// console.log("Username is invalid");
 			nameMessage.innerHTML = "Name is invalid";  //you can be more specific but okay for now
+			totalErrors++;
 		}
+		
 
+
+//-------------------------------------------------------------------------------------------
+		var campusIsSelected = false;
+
+		// Loop over all the campus
+		for(var i=0;i<campusOptions.length; i++) {
+			// Check if this campus has benn selected
+			if( campusOptions[i].checked){
+				campusIsSelected = true;
+			
+			}
+		}
+		//If campusIsSelected is still false
+		if( campusIsSelected  == false ) {
+			campusMessage.innerHTML = "Please select a campus";
+			totalErrors++;
+
+		} else {
+			campusMessage.innerHTML = '';
+		}
+		// If the total errors is greater than 0
+		if (totalErrors > 0) {
 		// Stop the form from submitting
 		event.preventDefault();
-
+	}
+//-------------------------------------------------------------------------------------------
 	// alert('submitted');
 }
 
